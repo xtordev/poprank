7dfc0302f98021e3d894514ae951428258469f68
Xian Torres <xtordev@proton.me>
2024-08-07 09:43:19 +0800

    test

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 
 export const routes: Routes = [
-  {path: "", component:HomeComponent},
+  {path: "home", component:HomeComponent},
   {path:"leaderboard",component:LeaderboardComponent},
   {path:"search",component:SearchComponent}
 ];
